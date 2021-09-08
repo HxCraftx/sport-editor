ba83a837fd3c149de63b7b8d7013acad5ba5c174
HxCraftx <50258446+HxCraftx@users.noreply.github.com>
2021-09-08 19:33:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import { run } from "./src/main.js";
 
-const DEFAULT_STEP_SIZE = "5000-15000";
+const DEFAULT_STEP_SIZE = "18000-20000";
 
 // 获取环境变量
 const config = {
-  username: process.env.XIAOMI_AMAZFIT_USERNAME,
-  password: process.env.XIAOMI_AMAZFIT_PASSWORD,
+  username: process.env.13471004073,
+  password: process.env.llkst4499.,
   user_id: process.env.XIAOMI_AMAZFIT_USER_ID,
   app_token: process.env.XIAOMI_AMAZFIT_APP_TOKEN,
   step_size: process.env.STED_SIZE_RANGE ?? DEFAULT_STEP_SIZE,
